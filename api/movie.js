f4219198fd3977d1b22b157f1cca992f6f449bf6
as-zini <aszini@naver.com>
2024-02-24 22:50:48 +0900

    add environment variables

--- api/movie.js
+++ api/movie.js
@@ -1,11 +1,13 @@
 import fetch from "node-fetch"
 
+const {APIKEY} = process.env
+
 //vercel의 서버리스펑션
 export default async function handler (request, response) {
   const {title, page, id} = JSON.parse(request.body)
   const url = id
-    ? `https://omdbapi.com?apikey=7035c60c&i=${id}&plot=full`
-    : `https://omdbapi.com?apikey=7035c60c&s=${title}&page=${page}  `
+    ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`
+    : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}  `
   const res = await fetch(url)
   const json = await res.json()
   response.status(200).json(json)
