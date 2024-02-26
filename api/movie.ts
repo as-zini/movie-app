ba7456953f0259822d44cb2753ac3fe597be25b4
as-zini <aszini@naver.com>
2024-02-26 19:32:19 +0900

    Compiled TypeScript

--- api/movie.ts
+++ api/movie.ts
@@ -1,9 +1,10 @@
 import fetch from "node-fetch"
+import {VercelRequest, VercelResponse} from '@vercel/node' // 버셀서버에서 사용할 수 있는 타입들을 가져오는 코드
 
 const {APIKEY} = process.env
 
 //vercel의 서버리스펑션
-export default async function handler (request, response) {
+export default async function handler (request: VercelRequest, response: VercelResponse) {
   const {title, page, id} = JSON.parse(request.body)
   const url = id
     ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`
--- src/store/about.ts
+++ src/store/about.ts
@@ -1,6 +1,14 @@
 import { Store } from "../core/fundamental"
 
-export default new Store({
+interface State {
+  photo: string
+  name: string
+  email: string
+  blog: string
+  github: string
+  repository: string
+}
+export default new Store<State>({
   photo: '../../zini_logo.png',
   name: 'ZINI / HwangJiHyeon',
   email: 'aszini@naver.com',
